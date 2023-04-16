3d14568856aad9b2512d63677bfab12c3f730fe5
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-16 13:42:35 -0300

    Script mega simples pra simular o player pulando

---
+++ Assets/Scripts/Player/StateMachine/Actions/SimulateJumping.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Player.StateMachine.Actions {
+    public class SimulateJumping : MonoBehaviour {
+        private void OnEnable() {
+            transform.parent.parent.localScale *= 1.5f;
+        }
+
+        private void OnDisable() {
+            transform.parent.parent.localScale /= 1.5f;
+        }
+    }
+}
