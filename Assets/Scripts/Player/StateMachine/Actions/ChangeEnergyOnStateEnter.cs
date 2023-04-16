7aaf16145012e6bc9370e6668d6180ee52116afe
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-16 16:12:08 -0300

    Corrigido bug em que ao apertar espaço varias vezes nao att o singleton de energia corretamente

---
+++ Assets/Scripts/Player/StateMachine/Actions/ChangeEnergyOnStateEnter.cs
@@ -0,0 +1,18 @@
+﻿using Player.EnergySystem;
+using UnityEngine;
+
+namespace Player.StateMachine.Actions {
+    public class ChangeEnergyOnStateEnter : MonoBehaviour {
+        [SerializeField] private int deltaValueOnEnable;
+        private EnergyManager _energyManager;
+
+
+        private void Awake() {
+            _energyManager = FindObjectOfType<EnergyManager>();
+        }
+
+        private void OnEnable() {
+            _energyManager.CurrentEnergy += deltaValueOnEnable;
+        }
+    }
+}
