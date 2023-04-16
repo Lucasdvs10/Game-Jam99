8563d913be61fbda6f28693ac2817296aba81546
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-16 18:54:34 -0300

    Girando o peixe

--- Assets/Scripts/Player/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement/PlayerMovement.cs
@@ -4,6 +4,7 @@ namespace PlayerMovement {
     public class PlayerMovement : MonoBehaviour {
         [SerializeField] float _playerMovementSpeed;
         [SerializeField] private bool _mantainInertia;
+        [SerializeField] private Transform _spriteTransform;
         
         private Rigidbody2D _playerRgb;
         private Transform _playerTransform;
@@ -15,8 +16,29 @@ namespace PlayerMovement {
 
         public void MovePlayer(Vector2 direction) {
             _playerRgb.velocity = _playerTransform.localToWorldMatrix * (direction * _playerMovementSpeed);
+            RotateSprite(direction);
         }
 
+        public void RotateSprite(Vector2 direction) {
+            if (direction == Vector2.up)
+                _spriteTransform.localRotation = Quaternion.Euler(0, 0, 90f);
+            
+            if (direction == Vector2.down)
+                _spriteTransform.localRotation = Quaternion.Euler(0, 0, -90f);
+            
+            if (direction == Vector2.right)
+                _spriteTransform.localRotation = Quaternion.Euler(0, 0, 0f);
+            
+            if (direction == Vector2.left)
+                _spriteTransform.localRotation = Quaternion.Euler(0, 0, 0f);
+            
+            // if (direction == new Vector2(0.71f, 0.71f)) Não sei pq não está funcionando
+            //     _spriteTransform.localRotation = Quaternion.Euler(0,0,45f);
+            //
+            // if (direction == new Vector2(0.71f,-0.71f))
+            //     _spriteTransform.localRotation = Quaternion.Euler(0,0,-45f);
+        }
+        
         public void StopPlayerMovement() {
             if(_mantainInertia) return;
             
