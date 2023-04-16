bb75166ce1daf23b2eda66f2b7ee006d16962d1e
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-16 00:50:35 -0300

    Fiz um input manager baseado em Unity events

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace Input {
+    public class InputManager : MonoBehaviour {
+        public UnityEvent<Vector2> MovementButtonPressedEvent;
+        public UnityEvent<Vector2> MovementButtonReleasedEvent;
+        public UnityEvent<Vector2> JumpButtonPressedEvent;
+        public UnityEvent<Vector2> JumpButtonReleasedEvent;
+        
+        public void ReadMovementKeys(InputAction.CallbackContext ctx) {
+            if (ctx.performed) {
+                MovementButtonPressedEvent.Invoke(ctx.ReadValue<Vector2>());
+            }
+            
+            if (ctx.canceled) {
+                MovementButtonReleasedEvent.Invoke(ctx.ReadValue<Vector2>());
+            }
+        }
+
+        public void ReadJumpKeys(InputAction.CallbackContext ctx) {
+            if (ctx.performed) {
+                JumpButtonPressedEvent.Invoke(ctx.ReadValue<Vector2>());
+            }
+
+            if (ctx.canceled) {
+                JumpButtonReleasedEvent.Invoke(ctx.ReadValue<Vector2>());
+            }
+        }
+    }
+}
