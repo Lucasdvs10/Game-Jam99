9ed8ec82b6e62b2aebdafd080016297cfd53a8c7
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-16 01:12:38 -0300

    Movimentação simples implementada

---
+++ Assets/Scripts/Input/TestEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Input {
+    public class TestEvent : MonoBehaviour {
+        public void Test() {
+            print("O evento foi chamado!");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace PlayerMovement {
+    public class PlayerMovement : MonoBehaviour {
+        [SerializeField] float _playerMovementSpeed;
+        [SerializeField] private bool _mantainInertia;
+        
+        private Rigidbody2D _playerRgb;
+        private Transform _playerTransform;
+
+        private void Awake() {
+            _playerRgb = GetComponentInParent<Rigidbody2D>();
+            _playerTransform = _playerRgb.transform;
+        }
+
+        public void MovePlayer(Vector2 direction) {
+            _playerRgb.velocity = _playerTransform.localToWorldMatrix * (direction * _playerMovementSpeed);
+        }
+
+        public void StopPlayerMovement() {
+            if(_mantainInertia) return;
+            
+            _playerRgb.velocity = Vector2.zero;
+        }
+        
+    }
+}
