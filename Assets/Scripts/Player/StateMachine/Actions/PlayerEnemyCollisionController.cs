3536083048a657a4f6e6eec209cb1e417fa7b0c6
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-17 10:26:26 -0300

    Corrigido bug em que era possivel sair do mapa enquanto voava

---
+++ Assets/Scripts/Player/StateMachine/Actions/PlayerEnemyCollisionController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Player.StateMachine.Actions {
+    public class PlayerEnemyCollisionController : MonoBehaviour {
+        public void SwitchPlayerEnemyCollision(bool newValue) {
+            if (newValue)
+                transform.parent.parent.gameObject.layer = 7;
+            else
+                transform.parent.parent.gameObject.layer = 8;
+        }
+        
+    }
+}
