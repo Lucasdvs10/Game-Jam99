38f7aaf69c3543b826c4e849933afa62907dee0b
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-18 13:00:56 -0300

    01 Montando fases
    Montei as fases 1 e 2 usando os tiles temporários

---
+++ Assets/Scripts/GameManager/LoadSceneOnTriggerEnter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace GameManager {
+    [RequireComponent(typeof(SceneLoader))]
+    public class LoadSceneOnTriggerEnter : MonoBehaviour {
+        public string SceneName;
+        private SceneLoader _sceneLoader;
+
+        private void Awake() {
+            _sceneLoader = GetComponent<SceneLoader>();
+        }
+
+
+        private void OnTriggerEnter2D(Collider2D col) {
+            _sceneLoader.LoadScene(SceneName);
+        }
+    }
+}
