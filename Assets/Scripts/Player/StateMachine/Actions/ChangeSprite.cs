bee270674b88ab1749724f0fb843e9d85769990b
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-19 18:23:01 -0300

    Agora o sprite do peixe muda dinamicamente
    Para cada estado do peixe, temos um sprite diferente. Por exemplo, no estado submerso ele está com as asas fechadas.
    
    Tbm alterei a prioridade dele nas layers dinamicamente, portanto quando estiver submerso, ele aparece atrás da água e não em cima

---
+++ Assets/Scripts/Player/StateMachine/Actions/ChangeSprite.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Player.StateMachine.Actions {
+    public class ChangeSprite : MonoBehaviour {
+        private SpriteRenderer _spriteRenderer;
+        
+        private void Awake() {
+            _spriteRenderer = transform.parent.parent.GetComponentInChildren<SpriteRenderer>();
+        }
+
+        public void SetNewSprite(Sprite newSprite) {
+            Awake();
+            _spriteRenderer.sprite = newSprite;
+        } 
+    }
+}
---
+++ Assets/Scripts/Player/StateMachine/Actions/ChangeSpriteLayerPriority.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Player.StateMachine.Actions {
+    public class ChangeSpriteLayerPriority : MonoBehaviour {
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake() {
+            _spriteRenderer = transform.parent.parent.GetComponentInChildren<SpriteRenderer>();
+        }
+
+        public void SetPriorityValueOnLayers(int newValue) {
+            Awake();
+            _spriteRenderer.sortingOrder = newValue;  
+        } 
+    }
+}
