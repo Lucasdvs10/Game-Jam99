5cefe72561f2a880242576e51eb57fb828229069
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-20 11:03:57 -0300

    1.1 Inimigos - Mock do ataque da cobra
    Fiz um script mockado para simular o bote da cobra. Quando tivermos mais componentes para o bote, como por exemplo animação, efeito sonoro e etc, já está na mão

---
+++ Assets/Scripts/Enemies/Snake/SnakeAttack.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Enemies.Snake {
+    public class SnakeAttack : MonoBehaviour {
+        public void Attack() {
+            print("Cobra dando o bote");
+        }
+    }
+}
