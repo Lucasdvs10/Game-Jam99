a0994ac7f07ea8c1ff74d3958e5fdf21d789fe42
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-20 11:59:36 -0300

    Importei a biblioteca NavMesh plus

---
+++ Assets/Scripts/Enemies/Aligator/MovementAI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemies.Aligator {
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class MovementAI : MonoBehaviour {
+        private NavMeshAgent _agent;
+        private Vector3 _playerPosition;
+        
+        private void Awake() {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        [ContextMenu("Mover até a posicao atual do player")]
+        public void GoToPlayerPosition() {
+            GetPlayerPosition();
+            _agent.SetDestination(_playerPosition);
+        }
+
+        private void GetPlayerPosition() {
+            _playerPosition = GameObject.FindWithTag("Player").transform.position;
+        }
+    }
+}
