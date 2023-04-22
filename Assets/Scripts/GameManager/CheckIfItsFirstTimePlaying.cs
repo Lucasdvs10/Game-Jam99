e1d6b72a2d314cb1f3fabec99cfca3944cae5b8b
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-22 18:10:51 -0300

    7 Montando fases - First Time Experience
    Ao abrir o jogo verifica-se se é a primeira vez jogando. Se for, a janela do aquário estará fechada até o jogador testar todos os controles.
    
    Se não for, a janela já estará aberta e o jogador poderá sair do consultório

---
+++ Assets/Scripts/GameManager/CheckIfItsFirstTimePlaying.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace GameManager {
+    public class CheckIfItsFirstTimePlaying : MonoBehaviour {
+
+        public bool Check() {
+            if (PlayerPrefs.GetInt("FIRSTTIMEOPENING", 1) == 1) {
+                Debug.Log("First Time Opening");
+
+                //Set first time opening to false
+                PlayerPrefs.SetInt("FIRSTTIMEOPENING", 0);
+
+                return true;
+
+            }
+            else {
+                Debug.Log("NOT First Time Opening");
+
+                return false;
+            }
+        }
+
+        public void Kappa() {
+
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager/FirstTimeExperienceManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace GameManager {
+    [RequireComponent(typeof(CheckIfItsFirstTimePlaying))]
+    public class FirstTimeExperienceManager : MonoBehaviour {
+        [SerializeField] private SpriteRenderer _spriteRendererBackground;
+        [SerializeField] private Sprite _firstTimePlayingSprite;
+        [SerializeField] private Sprite _defaultSprite;
+        private CheckIfItsFirstTimePlaying _checkIfItsFirstTime;
+
+        private bool[] FirstTimeChecksArray = {false, false, false, false, false, false, false};
+        
+        private void Awake() {
+            _checkIfItsFirstTime = GetComponent<CheckIfItsFirstTimePlaying>();
+        }
+
+        public void UpdateFirstTimeChecksArray(int index) {
+            FirstTimeChecksArray[index] = true;
+            VerifyIfFirstTimeExperienceIsOver();
+        }
+
+        public void UpdateFirstTimeChecksArray(Vector2 direction) {
+            if(direction == Vector2.up) UpdateFirstTimeChecksArray(3);
+            if(direction == Vector2.down) UpdateFirstTimeChecksArray(4);
+            if(direction == Vector2.right) UpdateFirstTimeChecksArray(5);
+            if(direction == Vector2.left) UpdateFirstTimeChecksArray(6);
+        }
+
+        public void VerifyIfFirstTimeExperienceIsOver() {
+            _spriteRendererBackground.sprite = _firstTimePlayingSprite;
+            
+            foreach (var check in FirstTimeChecksArray) {
+                if (!check) {
+                    return;
+                }
+            }
+            
+            _spriteRendererBackground.sprite = _defaultSprite;
+        }
+
+        private void Start() {
+            if (!_checkIfItsFirstTime.Check()) {
+                _spriteRendererBackground.sprite = _defaultSprite;
+            }
+            
+            else
+                VerifyIfFirstTimeExperienceIsOver();
+        }
+    }
+}
