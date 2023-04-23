e05e0895a1cbcfa62dd3b4aca8cddbf15f9ba530
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-23 12:46:51 -0300

    Começando a colocar efeito sonoro
    Coloquei efeito sonoro para quando o peixe está nadando na superficie e quando está nadando na profundeza

---
+++ Assets/Scripts/PlaySoundEffect.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class PlaySoundEffect : MonoBehaviour {
+        [SerializeField] private AudioClip[] _audioClipsArray;
+        [SerializeField] private AudioSource _audioSource;
+        
+        private int _currentIndex;
+        
+        public void StartPlayAudiosLoopCO() {
+            StopPlayAudiosCO();
+            StartCoroutine(PlayAudiosLoop());
+        }
+
+        public void StopPlayAudiosCO() {
+            StopAllCoroutines();
+        }
+        
+        private IEnumerator PlayAudiosLoop() {
+            while (true) {
+                PlayNextAudioInArray();
+                yield return new WaitForSeconds(_audioSource.clip.length);
+            }
+        }
+        
+        public void PlayNextAudioInArray() {
+            CurrentIndex++;
+            _audioSource.clip = _audioClipsArray[_currentIndex];
+            _audioSource.Play();
+        }
+
+
+        public int CurrentIndex {
+            get => _currentIndex;
+            set {
+                if (value >= _audioClipsArray.Length)
+                    _currentIndex = 0;
+                else {
+                    _currentIndex = value;
+                }
+            }
+        }
+    }
+}
