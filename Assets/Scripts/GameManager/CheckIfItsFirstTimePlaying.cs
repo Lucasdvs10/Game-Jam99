529ff121ff24506965abd694d7cebde7039f420e
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-04-23 23:07:17 -0300

    Coloquei a logo, fiz o pescador e coloquei sprite no jacaré

--- Assets/Scripts/GameManager/CheckIfItsFirstTimePlaying.cs
+++ Assets/Scripts/GameManager/CheckIfItsFirstTimePlaying.cs
@@ -4,15 +4,13 @@ namespace GameManager {
     public class CheckIfItsFirstTimePlaying : MonoBehaviour {
 
         public bool Check() {
-            if (PlayerPrefs.GetInt("FIRSTTIMEOPENING", 1) == 1) {
-                PlayerPrefs.SetInt("FIRSTTIMEOPENING", 1);
+            if (!PlayerPrefs.HasKey("FIRSTTIMEOPENING")) {
+                PlayerPrefs.SetInt("FIRSTTIMEOPENING", 0);
 
                 return true;
 
             }
-
             return false;
-
             // return true;
         }
 
